2a7e65f22641f491291b11777b3d8e26a3527d6e
tatar4950 <37213@bpk.ac.th>
2025-06-27 21:34:43 +0700

    Add files via upload

---
+++ คำนวนset.py
@@ -0,0 +1,7 @@
+a = {10,20,30,40,50 }
+b ={30,40,50,60,70,80}
+
+union_result = a.union(b)
+intersection_result = a.intersection(b)
+difference_result = a.difference(b)
+symmetric_difference_result = a.symmetric_difference(b)
