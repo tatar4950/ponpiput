f3a2bd18da9d4cdb27fc8f953564423aecee5de2
tatar4950 <37213@bpk.ac.th>
2025-06-30 16:50:50 +0700

    Add files via upload

---
+++ ไฟจราจร.py
@@ -0,0 +1,8 @@
+traffic_light = input("Enter the traffic light color (red/green): ")
+
+if traffic_light == "red":
+    print("Stop the car")
+elif traffic_light == "green":
+    print("You can go")
+else:
+    print("Invalid traffic light color")
