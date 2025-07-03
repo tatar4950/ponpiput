4b0e3605193bffbbb01a052db72bd820aab38ee3
tatar4950 <37213@bpk.ac.th>
2025-07-03 20:37:34 +0700

    Add files via upload

---
+++ คำนวนราคาสินค้า .py
@@ -0,0 +1,19 @@
+"one kilo of pork" == 150.00
+"oneChicken kilo of  breast meat" == 105.00
+"one chiken" == 120.00
+"Mama Tom Yum"== 6.50
+"one kilo of rice" == 150.00
+"sugar"== 20.00
+"Three Cooks Canned Fish"== 10.00
+"Oyster Sauce"== 18.00
+"MSG"==10.25
+"egg"==120.25
+"green tea"==21.50
+"pepsi"==29.50
+"cofee"==15.75
+"butterbread" == 19.00
+"tea"==11.50
+"water"==15.15
+"ice"==10
+
+print("one kilo of pork"+"oneChicken kilo of  breast meat"+"one chiken"+"Mama Tom Yum"+"one kilo of rice"+"one kilo of rice"+"sugar"+"Three Cooks Canned Fish"+"Oyster Sauce"+"MSG"+"egg"+"green tea"+"pepsi"+"cofee"+"butterbread"+"tea")
