ca8e37cdcea10e9507e3f5f590e0fbff7990ec69
tatar4950 <37213@bpk.ac.th>
2025-07-03 20:51:53 +0700

    Add files via upload

---
+++ ระบบ log in.py
@@ -0,0 +1,11 @@
+USERNAME = "admin"
+PASSWORD = ("ad13n@23g")
+
+input_username = input("Username: ")
+input_password = input("Password: ")
+
+if input_username == USERNAME and input_password == PASSWORD:
+    print(" Login successful!")
+else:
+    print(" Invalid username or password.")
+    
