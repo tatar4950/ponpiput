eb8f6a7022dd6100533cce26e499a8d1d1ebcb91
tatar4950 <37213@bpk.ac.th>
2025-08-04 15:05:27 +0700

    Add files via upload

---
+++ str .py
@@ -0,0 +1,9 @@
+def concat_strings(str1, str2, str3):
+    result = str1 + str2 + str3
+    return result
+
+s1 = "Hello, "
+s2 = "world"
+s3 = "!"
+combined = concat_strings(s1, s2, s3)
+print("Combined string:", combined)
